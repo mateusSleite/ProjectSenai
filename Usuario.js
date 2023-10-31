6574d6b39844245ca3d09e76ff52a4572de46bb0
unknown <mateusleite2709@gmail.com>
2023-10-31 11:12:11 -0300

    Componente

--- Usuario.js
+++ Usuario.js
@@ -4,23 +4,31 @@ import { UtilsContexto } from "./Context";
 
 export default function Usuario(props) {
 
-    const {utils, setContexto} = useContext(UtilsContexto)
+    const { utils, setContexto } = useContext(UtilsContexto)
 
 
     return (
-        <View style={{ alignItems: "center", backgroundColor: "lightgray" }}>
-            <View style={{ marginTop: "50px"}}>
-            <Text style={{ fontSize: "70px"}}>Usuarios </Text>
+        <View style={{ alignItems: "center", backgroundColor: "lightgray", height: "100%" }}>
+            <View style={{ marginTop: "50px" }}>
+                <Text style={{ fontSize: "70px" }}>Usuarios </Text>
             </View>
-            <View style={{border: "solid 1px black", width: "500px", height: "100px", borderRadius: "10px", marginBottom:"689px"}}>
-                <View style={{marginTop: "5px", marginLeft: "10px"}}>
-                <Text>Nome: {utils.nome} </Text>
-                <Text>Idade: {utils.idade} </Text>
-                <Text>Sexo: {utils.sexo} </Text>
-                <Text>Email: {utils.email} </Text>
-                </View>
+            {container(utils)}
+            {container(utils)}
 
-            </View>
         </View>
     );
+}
+
+function container(props) {
+
+    return (
+        <View style={{ border: "solid 1px black", width: "500px", height: "100px", borderRadius: "10px", marginBottom: "20px" }}>
+            <View style={{ marginTop: "5px", marginLeft: "10px" }}>
+                <Text>Nome: {props.nome} </Text>
+                <Text>Idade: {props.idade} </Text>
+                <Text>Sexo: {props.sexo} </Text>
+                <Text>Email: {props.email} </Text>
+            </View>
+        </View>
+    )
 }
